09f17c761bd38e5bec217926774963357f6442fb
Siwakorn Noisundod <jew.siwakorn14@gmail.com>
2024-05-20 11:54:42 +0700

    upload only ts file

--- clockAngle.js
+++
@@ -1,8 +0,0 @@
-function getClockAngle(hh_mm) {
-    var _a = hh_mm.split(":").map(Number), hour = _a[0], minute = _a[1];
-    var minuteAngle = minute * 6;
-    var hourAngle = (hour % 12) * 30 + minute / 2;
-    var angle = Math.abs(hourAngle - minuteAngle);
-    angle = Math.min(angle, 360 - angle);
-    return angle <= 180 ? angle : 360 - angle;
-}
